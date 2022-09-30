dbe1208dc35aa02fc3d21be9f46e5700e4814ff8
Agureeva Anna <agureeva@ro.ru>
2022-09-30 21:20:44 +0300

    Удалила метод компоратор, в main реализовала его через lambda

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,19 @@ public class Main {
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
         people.add(new Person("Илья", "Потемкин-Таврический", 12));
-        people.add(new Person("Олег", "Илларионов-Голеншцев-Кутузов", 8));
+        boolean олег = people.add(new Person("Олег", "Илларионов-Голеншцев-Кутузов", 8));
         people.add(new Person("Андрей", "Коновалов", 15));
         people.add(new Person("Никита", "Мартынюк", 7));
         people.add(new Person("Антон", "Орлов-Чесменский", 19));
         people.add(new Person("Федор", "Березьев", 20));
 
         System.out.println(people);
-        Collections.sort(people, new PersonStatusComporator());
+        Collections.sort(people, (o1, o2) -> {
+            if (Integer.compare(Person.getWordCount(o1.getSurname()), Person.getWordCount(o2.getSurname())) != 0) {
+                return Integer.compare(Person.getWordCount(o1.getSurname()), Person.getWordCount(o2.getSurname()));
+            }
+            return Integer.compare(o1.getAge(), o2.getAge());
+        });
         System.out.println(people);
     }
 }
